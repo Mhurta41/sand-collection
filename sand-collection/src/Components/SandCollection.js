b3d7d3c97e3984651b18a6cf2b2ce4552b7ac0f7
Mhurta41 <Mhurta41@gmail.com>
2021-06-22 19:51:24 -0500

    made sand collection page mobile friendly

--- sand-collection/src/Components/SandCollection.js
+++ sand-collection/src/Components/SandCollection.js
@@ -6,7 +6,7 @@ const SandCollection = () => {
 		<div className='cards-container'>
 			<div className='card'>
 				<Link to='/destination' className='card-link'>
-					<h1>Destination</h1>
+					<h1>Myrtle Beach, South Carolina</h1>
 					<img
 						className='sand-image'
 						src='https://i.imgur.com/I1yc66c.jpeg'
@@ -16,7 +16,7 @@ const SandCollection = () => {
 			</div>
 			<div className='card'>
 				<Link to='/destination' className='card-link'>
-					<h1>Destination</h1>
+					<h1>New Orleans, Louisiana</h1>
 					<img
 						className='sand-image'
 						src='https://i.imgur.com/I1yc66c.jpeg'
@@ -26,7 +26,7 @@ const SandCollection = () => {
 			</div>
 			<div className='card'>
 				<Link to='/destination' className='card-link'>
-					<h1>Destination</h1>
+					<h1>Cozumel, Mexico</h1>
 					<img
 						className='sand-image'
 						src='https://i.imgur.com/I1yc66c.jpeg'
@@ -36,7 +36,47 @@ const SandCollection = () => {
 			</div>
 			<div className='card'>
 				<Link to='/destination' className='card-link'>
-					<h1>Destination</h1>
+					<h1>Montevideo, Nicaragua</h1>
+					<img
+						className='sand-image'
+						src='https://i.imgur.com/I1yc66c.jpeg'
+						alt='sample'
+					/>
+				</Link>
+			</div>
+			<div className='card'>
+				<Link to='/destination' className='card-link'>
+					<h1>Lima, Peru</h1>
+					<img
+						className='sand-image'
+						src='https://i.imgur.com/I1yc66c.jpeg'
+						alt='sample'
+					/>
+				</Link>
+			</div>
+			<div className='card'>
+				<Link to='/destination' className='card-link'>
+					<h1>Miami, Florida</h1>
+					<img
+						className='sand-image'
+						src='https://i.imgur.com/I1yc66c.jpeg'
+						alt='sample'
+					/>
+				</Link>
+			</div>
+			<div className='card'>
+				<Link to='/destination' className='card-link'>
+					<h1>Carillo, Costa Rica</h1>
+					<img
+						className='sand-image'
+						src='https://i.imgur.com/I1yc66c.jpeg'
+						alt='sample'
+					/>
+				</Link>
+			</div>
+			<div className='card'>
+				<Link to='/destination' className='card-link'>
+					<h1>Punta Cana, Dominican Republic</h1>
 					<img
 						className='sand-image'
 						src='https://i.imgur.com/I1yc66c.jpeg'
